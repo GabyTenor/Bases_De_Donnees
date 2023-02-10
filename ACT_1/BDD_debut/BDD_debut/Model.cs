5fa98f6b782d7a31af6c238a81ea77eca5d9fd75
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2023-02-10 14:23:55 +0100

    Chercher un société

---
+++ ACT_1/BDD_debut/BDD_debut/Model.cs
@@ -0,0 +1,63 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+
+
+namespace BDD_debut
+{
+    public struct Model
+    {
+        public string DefinirCheminBD()
+        {
+            string CheminBDRelatif = System.IO.Directory.GetCurrentDirectory() + "\\Comptoirs.accdb";
+
+            return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + CheminBDRelatif;
+        }
+        public string ChercherClientSociete(string societe)
+        {
+            string resultat = "";
+            DataSet sortie = new DataSet();
+            string query = "SELECT * FROM Clients WHERE Société=\"" + societe + "\";";
+
+            try
+            {
+                OleDbConnection maBDD = new OleDbConnection(DefinirCheminBD());
+                maBDD.Open();
+
+                OleDbDataAdapter traducteur = new OleDbDataAdapter(query, maBDD);
+
+                traducteur.Fill(sortie);
+
+                for (int i = 0; i < sortie.Tables[0].Rows.Count; i++)
+                {
+                    resultat += sortie.Tables[0].Rows[i]["Code Client"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Société"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Contact"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Fonction"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Adresse"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Ville"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Région"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Pays"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Téléphone"].ToString() + " | " +
+                                sortie.Tables[0].Rows[i]["Fax"].ToString() + " | " + "\n";
+                }
+
+
+                maBDD.Close();
+            }
+            catch (Exception ex)
+            {
+                resultat = ex.Message;
+                throw;
+            }
+
+            return resultat;
+        }
+
+    }
+}       
---
+++ ACT_1/BDD_debut/BDD_debut/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_debut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Model methodesBDD = new Model();
+            string demande;
+            string refaire = "";
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Tapez un nom d'entreprise pour y retrouver tous ses clients (Exemple : France restauration   Blondel père et fils   Around the Horn   Ana Trujillo Emparedados y helados");
+                    demande = Console.ReadLine();
+                } while (demande == "");
+
+                Console.WriteLine(methodesBDD.ChercherClientSociete(demande));
+
+                Console.WriteLine("Recommencer? Espace si oui");
+                refaire = Console.ReadLine();
+                
+                Console.Clear();
+            } while (refaire == " ");
+
+            Console.ReadLine();
+        }
+    }
+}

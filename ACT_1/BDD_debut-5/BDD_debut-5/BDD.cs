fd256e8d483824143b2c247c000f5eb7a3bddfb9
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2023-02-16 13:29:38 +0100

    Supprimer une ligne

---
+++ ACT_1/BDD_debut-5/BDD_debut-5/BDD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+
+namespace BDD_debut_5
+{
+    public struct BDD
+    {
+        public string DefinirCheminBD()
+        {
+            string CheminBDRelatif = System.IO.Directory.GetCurrentDirectory() + "\\Comptoirs.accdb";
+
+            return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + CheminBDRelatif;
+        }
+
+        public string SupprimerUneLigne(string nomCategorie)
+        {
+            string resultat = "";
+
+            try
+            {
+                string query = "DELETE FROM Catégories WHERE NomdeCategorie =\"" + nomCategorie + "\";";
+
+                OleDbConnection maBDD = new OleDbConnection(DefinirCheminBD());
+                maBDD.Open();
+                OleDbCommand command = new OleDbCommand(query, maBDD);
+                command.ExecuteNonQuery();
+                maBDD.Close();
+
+                resultat = "Suppression effectuée.";
+
+            }
+            catch (Exception ex)
+            {
+                resultat = ex.Message;
+            }
+
+            return resultat;
+        }
+    }
+}
--- ACT_1/BDD_debut-5/BDD_debut-5/Program.cs
+++ ACT_1/BDD_debut-5/BDD_debut-5/Program.cs
@@ -10,6 +10,14 @@ namespace BDD_debut_5
     {
         static void Main(string[] args)
         {
+            BDD pont = new BDD();
+
+            Console.WriteLine("Supprimer une ligne dans la table des catégories\n");
+            Console.WriteLine("Nom de catégorie à supprimer ?");
+            string nomCategorie = Console.ReadLine();
+            
+            Console.WriteLine(pont.SupprimerUneLigne(nomCategorie));
+            Console.ReadLine();
         }
     }
 }

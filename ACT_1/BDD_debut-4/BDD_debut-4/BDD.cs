88b023f0fa2d2774793ddd8f6a96078797d31392
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2023-02-16 13:19:45 +0100

    Montrer tout dans Catégorie

---
+++ ACT_1/BDD_debut-4/BDD_debut-4/BDD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+
+namespace BDD_debut_4
+{
+    public struct BDD
+    {
+        public string DefinirCheminBD()
+        {
+            string CheminBDRelatif = System.IO.Directory.GetCurrentDirectory() + "\\Comptoirs.accdb";
+
+            return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + CheminBDRelatif;
+        }
+
+        public string List()
+        {
+            string resultat = "";
+            DataSet sortie = new DataSet();
+            try
+            {            
+                string query = "Select NomdeCategorie FROM Catégories";
+
+                OleDbConnection maBDD = new OleDbConnection(DefinirCheminBD());           
+                OleDbDataAdapter messager = new OleDbDataAdapter(query, maBDD);
+                maBDD.Open();
+                messager.Fill(sortie);
+                maBDD.Close();
+
+                for (int i = 0; i < sortie.Tables[0].Rows.Count; i++)
+                {
+                    resultat += sortie.Tables[0].Rows[i]["NomdeCategorie"].ToString() + "\n";              
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resultat = ex.Message;
+            }
+
+            return resultat;
+        }
+    }
+}
---
+++ ACT_1/BDD_debut-4/BDD_debut-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_debut_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BDD pont = new BDD();
+
+            Console.WriteLine("Liste des catégories disponibles : ");
+            Console.WriteLine(pont.List());
+            Console.ReadLine();
+        }
+    }
+}
